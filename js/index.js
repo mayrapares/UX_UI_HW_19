2f7991b7aaf2eb30b14ac06bbd3df408df4993fd
mayrapares <69501819+mayrapares@users.noreply.github.com>
2021-02-09 08:25:38 -0500

    changes

---
+++ js/index.js
@@ -0,0 +1,15 @@
+console.log ("Your index.js file is loaded correctly!");
+$("h3").hover(function(){
+    $(this).css("background-color", "orange");
+    }, function(){
+    $(this).css("background-color", "#ffffff");
+  });
+
+  $("#about").on("click",function()){
+    <form action="index.html"> <input type="submit"></input> </form>
+  }
+
+  $("#work").on("click", function pageRedirect(){
+        window.location.href = "https://www.tutorialrepublic.com/";
+  }
+
